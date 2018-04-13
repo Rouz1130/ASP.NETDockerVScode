2e6c85dc35305db6cc196104b43f4dd81d24960b
Rouz Majlessi <rmajlessi@gmail.com>
2018-04-13 15:12:28 -0700

    Add RabbitMQ message broker container for communication to each microservices in teh docker.yml file

--- Services/Identity.Api/Startup.cs
+++ Services/Identity.Api/Startup.cs
@@ -24,6 +24,14 @@ namespace Identity.Api
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            
+
+             //By connecting here we are making sure that our service
+            //cannot start until redis is ready. This might slow down startup,
+            //but given that there is a delay on resolving the ip address
+            //and then creating the connection it seems reasonable to move
+            //that cost to startup instead of having the first request pay the
+            //penalty.
 
             services.AddSingelton(sp=> {
 
